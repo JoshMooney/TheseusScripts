9918cfa285eda13b5e6e43a6dd4b135e04f0e9c9
Josh <joshmoo2009@hotmail.com>
2017-09-17 22:12:29 +0100

    Adding aegon support

--- server_status.py
+++ server_status.py
@@ -35,9 +35,10 @@ check_service('nginx', {'name': 'NGINX', 'msg': 'Status of nginx'})
 check_service('smbd', {'name': 'SAMBA', 'msg': 'Status of Samba server'})
 check_service('transmission-daemon', {'name': 'TRANSMISSION', 'msg': 'Status of transmission-daemon'})
 
-aegon_status = subprocess.check_output("curl -v 192.168.0.87", shell=True)
+ping_response = subprocess.Popen(["/bin/ping", "-c1", "-w100", "192.168.0.87"], stdout=subprocess.PIPE).stdout.read()
+print ping_response
 aegon_status = False
-if ("HTTP/1.1 200 OK" in aegon_status):
+if ("HTTP/1.1 200 OK" in status):
 	aegon_status = True
 log('AEGON', 'NetGear NAS @192.168.0.87', aegon_status)
 row_log()
