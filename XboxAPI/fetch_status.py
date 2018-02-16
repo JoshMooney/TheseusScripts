ccada24bca46f4a42ba56b53175e124c39078347
Josh Mooney <joshmoo2009@hotmail.com>
2018-02-16 22:16:24 +0000

    Added Lab Muiltiplayer game

--- XboxAPI/fetch_status.py
+++ XboxAPI/fetch_status.py
@@ -2,5 +2,7 @@ from xboxapi import Client
 
 client = Client(api_key="963c54c1ade7e21efa9b2ce8c6aaac736d9f284f")
 gamer = client.gamer('Joshmoo2012')
+print(gamer)
 
 presence = gamer.get('presence')
+print(presence)
