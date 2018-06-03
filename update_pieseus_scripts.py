1c16492b2f2cd2f55bc0651330013e0f8d807217
JoshMooney <joshmoo2009@hotmail.com>
2018-06-03 00:13:22 +0000

    Finished update_pieseus_script.py

--- update_pieseus_scripts.py
+++ update_pieseus_scripts.py
@@ -8,21 +8,29 @@ import shutil
 
 
 scripts = [
-    {"name": "test_file.txt", "des": "/home/pi/.scripts.bin", "src": "/home/pi/Project/TheseusScripts"}
-    #{"name": "pi_server_startup.py", "des": "/home/pi/.scripts.bin", "src": "/home/pi/Project/TheseusScripts"},
-    #{"name": "pi_server_status.py", "des": "/home/pi/.scripts.bin", "src": "/home/pi/Project/TheseusScripts"}
+    {"name": "pi_server_startup.py", "des": "/home/pi/.scripts.bin", "src": "/home/pi/Project/TheseusScripts"},
+    {"name": "pi_server_status.py", "des": "/home/pi/.scripts.bin", "src": "/home/pi/Project/TheseusScripts"}
 ]
 
 print("** Updating Pieseus Scripts **")
 
 for script in scripts:
     try:
-        print("Copying %s" % script['name'])
-        src = "&s/%s" % script['src'], script['name']
-        des = "&s/%s" % script['des'], script['name']
+        print("    Copying {}".format(script['name']))
+        src = "{}/{}".format(script['src'], script['name'])
+        des = "{}/{}".format(script['des'], script['name'])
         shutil.copyfile(src, des)
+	print("    Finsihed copying {}".format(script['name']))
+	print("")
     except Exception as error:
-        print("Failed to copy %s" % script.name)
+	print("")
+        print("******************************************")
+	print("")
+	print("    Failed to copy %s" % script['name'])
+	print("    " + str(error))
+	print("")
+	print("******************************************")
+	print("")
 
 
 
