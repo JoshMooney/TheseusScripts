3c9c90be500a72090131ea68d10ede8830677036
joshmooney <joshmoo2009@hotmail.com>
2018-06-03 02:48:38 +0100

    Updated backup script

--- backup_torrents.py
+++ backup_torrents.py
@@ -4,8 +4,8 @@ import platform
 
 
 dirs = {
-    "src": {"Windows": "F:/", "Linux": "/mnt/ShortTerm/torrents"},
-    "dst": {"Windows": "None", "Linux": "/mnt/Aegon/Videos/"}
+    "src": {"Windows": "F:/test_dir/", "Linux": "/mnt/ShortTerm/torrents"},
+    "dst": {"Windows": "F:/", "Linux": "/mnt/Aegon/Videos/"}
 }
 finished = []
 ver = platform.system()
@@ -13,14 +13,16 @@ ignore_dirs = ["_incomplete", "In_Progress", "$RECYCLE.BIN"]
 
 files = [d for d in os.listdir(dirs['src'][ver])]
 files = filter(lambda x: x not in ignore_dirs, files)
+stats = {"pass": 0, "fail": 0}
 
 for f in files:
     if os.path.isdir(dirs['dst'][ver]):
         print f
-        #shutil.copyfile(dirs['src'][ver], dirs['dst'][ver])
+        shutil.copyfile(dirs['src'][ver], dirs['dst'][ver])
+        stats['pass'] += 1
     else:
         print("Destination directory {} was not accessible".format(dirs['dst'][ver]))
-    # try copy to Aegon if connected
+        stats['fail'] += 1
 
-#print progress
+print("** {} files copied, {} files failed **.".format(stats['pass'], stats['fail']))
 
