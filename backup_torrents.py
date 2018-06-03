dd3d44d35e6674a64e192ddec03fe6fe60f1b743
joshmooney <joshmoo2009@hotmail.com>
2018-06-03 02:20:13 +0100

    Created backup script

--- backup_torrents.py
+++ backup_torrents.py
@@ -1,18 +1,25 @@
 import shutil
 import os
+import platform
 
-src = "/home/pi/Project/TheseusScripts"
-des_dir = "Aegon somewhere"
 
+dirs = {
+    "src": {"Windows": "F:/", "Linux": "/mnt/ShortTerm/torrents"},
+    "dst": {"Windows": "None", "Linux": "/mnt/Aegon/Videos/"}
+}
 finished = []
+ver = platform.system()
+ignore_dirs = ["_incomplete", "In_Progress", "$RECYCLE.BIN"]
 
-files = [x[0] for x in os.walk(src)]
+files = [d for d in os.listdir(dirs['src'][ver])]
+files = filter(lambda x: x not in ignore_dirs, files)
 
 for f in files:
-    if "_incomplete" not in f and f is not src:
+    if os.path.isdir(dirs['dst'][ver]):
         print f
-
-    # Verify Aegon
+        #shutil.copyfile(dirs['src'][ver], dirs['dst'][ver])
+    else:
+        print("Destination directory {} was not accessible".format(dirs['dst'][ver]))
     # try copy to Aegon if connected
 
 #print progress
