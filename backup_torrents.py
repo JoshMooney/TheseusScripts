25f6ebb1858838ba1e295be01751b1efdc20fbba
joshmooney <joshmoo2009@hotmail.com>
2018-06-03 04:01:34 +0100

    Added terminal fallback copy

--- backup_torrents.py
+++ backup_torrents.py
@@ -1,6 +1,7 @@
 import shutil
 import os
 import platform
+import subprocess
 
 
 def already_exist(_dir):
@@ -29,12 +30,20 @@ for f in files:
             print("The file {} already exists in the directory {}".format(f, dst))
             stats['skipped'] += 1
             continue
-        if os.path.isdir(src):
-            shutil.copytree(src, dst, False, None)
-        else:
-            shutil.copyfile(src, dst)
-        print("Copied {} to {}".format(f, dirs['dst'][ver]))
-        stats['pass'] += 1
+        try:
+            if os.path.isdir(src):
+                shutil.copytree(src, dst, False, None)
+            else:
+                shutil.copyfile(src, dst)
+            print("Copied {} to {}".format(f, dirs['dst'][ver]))
+            stats['pass'] += 1
+        except OSError as error:
+            print("Error copying with shutil, attempting terminal")
+            try:
+                subprocess.call(['cp', src, dst])
+            except Exception as error:
+                print("Error copying with terminal, giving up")
+                stats['fail'] += 1
     else:
         print("Destination directory {} was not accessible".format(dirs['dst'][ver]))
         stats['fail'] += 1
